d49e4338cdb0f5de1058af8c7946e7bfc99c9dec
DanielValdes <monosaicol@gmail.com>
2020-09-07 17:30:11 -0500

    adding kepler adding button samples adding Kepler theme (change) adding WEFI 2019 Data

---
+++ src/components/SidePanelStyles.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const myCustomTheme = {
+	sidePanelBg: '#ffffff',
+	titleTextColor: '#000000',
+	sidePanelHeaderBg: '#500000',
+	subtextColorActive: '#2473bd'
+};
+
+export default myCustomTheme;
