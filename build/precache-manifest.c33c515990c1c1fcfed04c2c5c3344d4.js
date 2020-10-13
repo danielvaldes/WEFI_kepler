ce9ced6e4846112cf7bef1fb3fb1a664a6106a96
DanielValdes <monosaicol@gmail.com>
2020-10-13 16:08:20 -0500

    adding build sample

---
+++ build/precache-manifest.c33c515990c1c1fcfed04c2c5c3344d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "380bf2d826a51d4dbca8c40a55b4ed08",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c68b74e197c7d9694ca",
+    "url": "/static/css/main.f759e38f.chunk.css"
+  },
+  {
+    "revision": "9a2ee110efe436808300",
+    "url": "/static/js/2.3e077e62.chunk.js"
+  },
+  {
+    "revision": "1d06ece824ea906305cbe4918bb6ceae",
+    "url": "/static/js/2.3e077e62.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c68b74e197c7d9694ca",
+    "url": "/static/js/main.fbb43de8.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
