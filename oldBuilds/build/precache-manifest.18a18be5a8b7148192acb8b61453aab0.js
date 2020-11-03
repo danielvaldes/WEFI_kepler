af9e497220dfeec6cb6b7aa8d95f0b89d8eef79f
DanielValdes <monosaicol@gmail.com>
2020-11-02 20:06:40 -0600

    adding Current (11/2/2020) build as old build

---
+++ oldBuilds/build/precache-manifest.18a18be5a8b7148192acb8b61453aab0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dde2f3d54d2e8411f1e99630156a1d7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6823e39a6b4034754b15",
+    "url": "/static/css/main.44914509.chunk.css"
+  },
+  {
+    "revision": "d91200acecaa41517006",
+    "url": "/static/js/2.55f92575.chunk.js"
+  },
+  {
+    "revision": "16c639f602cdfaa2d1125e2ac0886399",
+    "url": "/static/js/2.55f92575.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6823e39a6b4034754b15",
+    "url": "/static/js/main.1be1ae7d.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "c3cccc672081fc0ca5cff94bd72017c6",
+    "url": "/static/media/logo00.c3cccc67.png"
+  }
+]);
